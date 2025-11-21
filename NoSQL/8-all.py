5366f6f92ae9363a1ef835967f4f8e7c61e805d6
kloekam <keg7341@gmail.com>
2025-11-21 23:48:23 +0100

    Added solution for task 8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A script that lists all documents in a collection"""
+
+
+def list_all(mongo_collection):
+    """A function that lists all documents in a collection"""
+    return list(mongo_collection.find())
